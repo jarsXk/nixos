ed6ba9c3ad02dcab7b8a70370cf892d6df8be935
jarsXk <95273554+jarsXk@users.noreply.github.com>
2026-09-21 16:21:54 +0300

    Update nix-check.sh

--- nix-check.sh
+++ nix-check.sh
@@ -6,11 +6,13 @@ else
     FLAKE=".#$1"
 fi
 
+echo $#
+echo $1
+echo $FLAKE
 
 nix --extra-experimental-features 'nix-command flakes' flake check
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo $FLAKE
 nixos-rebuild dry-build --flake $FLAKE
