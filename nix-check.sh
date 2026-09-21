dd675350427311998eb59025c0fb303db49c4dc3
jarsXk <95273554+jarsXk@users.noreply.github.com>
2026-09-21 16:20:49 +0300

    Update nix-check.sh

--- nix-check.sh
+++ nix-check.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+if [ "$#" -lt 1 ]; then
+    FLAKE=".#$nixmatevm"
+else
+    FLAKE=".#$1"
+fi
+
+
 nix --extra-experimental-features 'nix-command flakes' flake check
 if [ $? -ne 0 ]; then
     exit 1
 fi
-FLAKE=".#${1:nixmatevm}"
+
 echo $FLAKE
 nixos-rebuild dry-build --flake $FLAKE
