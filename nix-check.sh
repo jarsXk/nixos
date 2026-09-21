9b8b8a376489847abc9ef5c23a6449c26926e0fa
jarsXk <95273554+jarsXk@users.noreply.github.com>
2026-09-21 16:17:09 +0300

    Update nix-check.sh

--- nix-check.sh
+++ nix-check.sh
@@ -4,4 +4,5 @@ nix --extra-experimental-features 'nix-command flakes' flake check
 if [ $? -ne 0 ]; then
     exit 1
 fi
-nixos-rebuild dry-build --flake .#"${1:nixmatevm}"
+MACHINE=${1:nixmatevm}
+nixos-rebuild dry-build --flake .#$MACHINE
